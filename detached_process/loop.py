174514099092cc36e9ed30d50db8d4ff337520ed
Howard Zhu <zhc_howard@hotmail.com>
2020-09-21 23:02:34 +0800

    the process started by Command() will keep running even the main process has finished

---
+++ detached_process/loop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+
+seconds = 0
+while True:
+    print(f"{seconds} econds has past")
+    seconds += 2
+    time.sleep(2)
---
+++ detached_process/src/main.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::process::Command;
+
+fn main() {
+    println!("main thread start");
+    let mut command = Command::new("python3");
+    let mut python_file = env::current_dir().unwrap();
+    python_file.push("loop.py");
+    command.arg(python_file);
+    command.spawn().expect("Unknow Error");
+    // loop.py will keep running even the main thread exits
+}
