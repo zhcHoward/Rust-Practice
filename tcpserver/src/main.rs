2a217f256d7b5792c1066b4bef7bebbfd5fa5175
Howard Zhu <zhc_howard@hotmail.com>
2020-10-04 23:13:59 +0800

    add example of TcpListener.incoming()

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -6,6 +6,12 @@ fn main() {
     let mut buf = [0; 1024];
     let listener = TcpListener::bind("127.0.0.1:8192").expect("Failed to bind port 8192");
     println!("Waiting for connection...");
+    // for stream in listener.incoming() {
+    //     match stream {
+    //         Ok(s) => println!("new connection: {:?}", s),
+    //         Err(e) => println!("Error: {:?}", e),
+    //     }
+    // }
     let connection = listener.accept().unwrap();
     println!("Received connection from {:?}", connection.1);
     let mut stream = connection.0;
