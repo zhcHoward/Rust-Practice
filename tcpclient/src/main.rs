6705f734265a85ff8525e4c749a96091e1c8e9a2
Howard Zhu <zhc_howard@hotmail.com>
2020-10-11 14:21:18 +0800

    add more demo

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,8 +1,22 @@
-use std::io::Write;
+use std::io::{stdin, stdout, Read, Write};
 use std::net::TcpStream;
 
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:8192").expect("Failed to connect 8192");
-    let message = "Hello World!";
-    stream.write(message.as_bytes()).unwrap();
+    let mut buf = [0; 1024];
+    let mut len: usize;
+    loop {
+        stdout().write(b"Send: ").unwrap();
+        stdout().flush().unwrap();
+        len = stdin().read(&mut buf).unwrap();
+        len -= 1;
+        stream.write(&buf[..len]).unwrap();
+        if &buf[..len] == b"exit" {
+            break;
+        }
+        len = stream.read(&mut buf).unwrap();
+        stdout().write(b"Received: ").unwrap();
+        stdout().write(&buf[..len]).unwrap();
+        stdout().write(b"\n").unwrap();
+    }
 }
--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -6,18 +6,18 @@ fn main() {
     let mut buf = [0; 1024];
     let listener = TcpListener::bind("127.0.0.1:8192").expect("Failed to bind port 8192");
     println!("Waiting for connection...");
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(s) => println!("new connection: {:?}", s),
-    //         Err(e) => println!("Error: {:?}", e),
-    //     }
-    // }
-    let connection = listener.accept().unwrap();
-    println!("Received connection from {:?}", connection.1);
-    let mut stream = connection.0;
-    let size = stream.read(&mut buf).unwrap();
-    println!(
-        "Received message: {}",
-        str::from_utf8(&buf[..size]).unwrap()
-    )
+    for stream in listener.incoming() {
+        match stream {
+            Ok(s) => println!("new connection: {:?}", s),
+            Err(e) => println!("Error: {:?}", e),
+        }
+    }
+    // let connection = listener.accept().unwrap();
+    // println!("Received connection from {:?}", connection.1);
+    // let mut stream = connection.0;
+    // let size = stream.read(&mut buf).unwrap();
+    // println!(
+    //     "Received message: {}",
+    //     str::from_utf8(&buf[..size]).unwrap()
+    // )
 }
